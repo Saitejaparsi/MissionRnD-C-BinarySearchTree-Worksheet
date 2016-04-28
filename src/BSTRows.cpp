b8c0b72dd82161360a20791e340bbc7c927eac26
Saitejaparsi <Saitejaparsi@missionrnd.com>
2016-04-28 21:24:32 +0530

    Submit Commit

--- src/BSTRows.cpp
+++ src/BSTRows.cpp
@@ -30,8 +30,52 @@ struct node{
 };
 
 
-
+int hgt(struct node *root)
+{
+	int ht;
+	if (!root)
+		return 0;
+	else
+		ht = hgt(root->left) > hgt(root->right) ? hgt(root->left) : hgt(root->right);
+	return ht + 1;
+}
+void get_elements(struct node *root, int level, int *arr, int *index)
+{
+	if (!root)
+		return;
+	if (level == 1)
+	{
+		arr[*index] = root->data;
+		(*index) = (*index) + 1;
+	}
+	else
+	{
+		get_elements(root->right, level - 1, arr, index);
+		get_elements(root->left, level - 1, arr, index);
+	}
+}
+int get_tree_size(struct node *root)
+{
+	if (!root)
+		return 0;
+	else
+	{
+		return get_tree_size(root->left) + get_tree_size(root->right) + 1;
+	}
+}
 int* BSTRighttoLeftRows(struct node* root)
 {
-    return NULL;
+	int  ht, level, index = 0;
+	int *arr = (int *)malloc(get_tree_size(root)*sizeof(int));
+	if (!root)
+		return NULL;
+	else
+	{
+		ht = hgt(root);
+		for (level = 0; level < ht; level++)
+		{
+			get_elements(root, level + 1, arr, &index);
+		}
+	}
+	return arr;
 }
